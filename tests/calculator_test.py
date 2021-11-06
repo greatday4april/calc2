b9b2f96c16270933b2427cbfaf04e1b5de8fff34
April Li <greatday4april@gmail.com>
2021-11-06 15:55:00 -0700

    Reformat and add travis link

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,41 +2,49 @@
 import pytest
 from calculator.main import Calculator
 
+
 def test_calculator_result():
     """testing calculator result is 0"""
     calc = Calculator()
     assert calc.result == 0
 
+
 def test_calculator_add():
     """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
+    # Arrange by instantiating the calc class
     calc = Calculator()
-    #Act by calling the method to be tested
+    # Act by calling the method to be tested
     calc.add_number(4)
-    #Assert that the results are correct
+    # Assert that the results are correct
     assert calc.result == 4
+
 
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
     assert calc.get_result() == 0
 
+
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
+
+
 def test_calculator_multiply():
     """ tests multiplication of two numbers"""
     calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
+    result = calc.multiply_numbers(1, 2)
     assert result == 2
+
 
 def test_calculator_divide():
     """ tests division of two non-zero_numbers"""
     calc = Calculator()
     result = calc.divide_numbers(5, 2)
     assert result == 2.5
+
 
 def test_calculator_divide_by_zero():
     """ tests dividing by zero"""
